371ff728352dbb1bfa23acb58b335dd90828dd9e
Maverickrd007 <raghav.duggal.s69@kalvium.community>
2025-01-28 14:14:47 +0530

    Create Schema.js

---
+++ Schema.js
@@ -0,0 +1,66 @@
+
+const mongoose = require('mongoose');
+
+
+const blogPostSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 5,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      minlength: 50,
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+    category: {
+      type: String,
+      default: 'General',
+      trim: true,
+    },
+    likes: {
+      type: [String],
+      default: [],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+    },
+    comments: [commentSchema],
+  });
+ 
+  const commentSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    commentedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+module.exports = BlogPost;
+
