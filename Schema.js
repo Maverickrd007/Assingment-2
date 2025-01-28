d119b65df4bee55031766713d7c99041009cef59
Maverickrd007 <raghav.duggal.s69@kalvium.community>
2025-01-28 14:32:22 +0530

    Update Schema.js

--- Schema.js
+++ Schema.js
@@ -1,66 +1,64 @@
-
 const mongoose = require('mongoose');
 
+const CommentSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  message: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  commentedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const blogPostSchema = new mongoose.Schema({
-    title: {
-      type: String,
-      required: true,
-      unique: true,
-      minlength: 5,
-      trim: true,
-    },
-    content: {
-      type: String,
-      required: true,
-      minlength: 50,
-      trim: true,
-    },
-    author: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    tags: {
-      type: [String],
-      default: [],
-    },
-    category: {
-      type: String,
-      default: 'General',
-      trim: true,
-    },
-    likes: {
-      type: [String],
-      default: [],
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-    updatedAt: {
-      type: Date,
-    },
-    comments: [commentSchema],
-  });
- 
-  const commentSchema = new mongoose.Schema({
-    username: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    message: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    commentedAt: {
-      type: Date,
-      default: Date.now,
-    },
-  });
+const BlogPostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    trim: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    minlength: 50,
+  },
+  author: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  tags: {
+    type: [String],
+    default: [],
+  },
+  category: {
+    type: String,
+    default: 'General',
+    trim: true,
+  },
+  likes: {
+    type: [String],
+    default: [],
+  },
+  comments: [CommentSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+  },
+});
 
-const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
+
 module.exports = BlogPost;
-
